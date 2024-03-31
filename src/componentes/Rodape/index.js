157ff2dd41cda0a682fc85f60406914e34f0890a
¨LucasAlkmimBarros¨ <lucasalk2005@gmail.com>
2024-03-31 13:24:16 -0300

    updates finais

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,18 +1,36 @@
 import './Rodape.css'
 
 const Rodape = () => {
-    return (
-        <footer className="rodape">
-            <div className="icons">
-                <img className="icon" src="/imagens/fb.png" alt="Facebook Organo" />
-                <img className="icon" src="/imagens/ig.png" alt="Instagram Organo" />
-                <img className="icon" src="/imagens/tw.png" alt="Twitter Organo" />
-            </div>
-            <img className="logo" src="/imagens/logo.png" alt="Organo" />
-            <p>Feito por <a href="
+    return (<footer className="footer">
+        <section>
+            <ul>
+                <li>
+                    <a href="facebook.com" target="_blank">
+                        <img src="/imagens/fb.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="twitter.com" target="_blank">
+                        <img src="/imagens/tw.png" alt="" />
+                    </a>
+                </li>
+                <li>
+                    <a href="instagram.com" target="_blank">
+                        <img src="/imagens/ig.png" alt="" />
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>Feito por <a className="link" href="
             https://www.linkedin.com/in/lucas-alkmim-594087277/" target="_blank">Lucas Alkmim Barros</a></p>
-        </footer>
-    )
+        </section>
+    </footer>)
 }
 
-export default Rodape;
+export default Rodape
+
+
